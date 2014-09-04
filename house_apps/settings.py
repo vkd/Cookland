b4514fcee4478493b1e63101bf69ccdd360a3bb4
vkd <vkd.castle@gmail.com>
2014-09-04 22:08:02 +0400

    move db.sqllite3 in db folder

--- house_apps/settings.py
+++ house_apps/settings.py
@@ -25,7 +25,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'house_apps.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
     }
 }
 
