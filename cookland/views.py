aee2cc3e9ed4c092407507263eef95d5a9d2b781
vkd <vkd.castle@gmail.com>
2014-09-04 22:13:25 +0400

    add 404 page

--- cookland/views.py
+++ cookland/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.template import loader, Context
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
 from cookland.models import CookPost
 
 def index(request):
-	posts = CookPost.objects.all()
+	try:
+		posts = CookPost.objects.all()
+	except CookPost.DoesNotExist:
+		raise Http404
+
 	context = { 'posts': posts }
 	return render(request, 'index.html', context)
 	
