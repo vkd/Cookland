a4cc4801b75f735c864060385424e7c895a19d7d
vkd <vkd.castle@gmail.com>
2014-09-10 23:22:45 +0400

    move CookPostAdmin, add Ingredient

--- cookland/admin.py
+++ cookland/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from cookland.models import CookPost, CookPostAdmin
+from cookland.models import CookPost
+
+class CookPostAdmin(admin.ModelAdmin):
+	list_display = ('title',)
 
 # Register your models here.
 admin.site.register(CookPost, CookPostAdmin)
--- cookland/models.py
+++ cookland/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib import admin
 
+class Ingredient(models.Model):
+	name = models.CharField(max_length=150)
+
 # Create your models here.
 class CookPost(models.Model):
 	title = models.CharField(max_length=150)
@@ -9,6 +12,4 @@ class CookPost(models.Model):
 	#class Meta:
 	#	ordering = ('-timestamp',)
 
-class CookPostAdmin(admin.ModelAdmin):
-	list_display = ('title',)
 
