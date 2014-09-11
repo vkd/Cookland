7b6531b16a5e40b3baf228beaaab22b5e7697128
vkd <vkd.castle@gmail.com>
2014-09-11 22:59:53 +0400

    #3 add model of recipe

--- cookland/models.py
+++ cookland/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from django.contrib import admin
 
-class Ingredient(models.Model):
+class Produce(models.Model):
 	name = models.CharField(max_length=150)
+
+class Recipe(models.Model):
+	name = models.CharField(max_length=150)
+	ingredients = models.ManyToManyField(Produce, related_name='recipes', through='Ingredient')
+	discribe = models.TextField()
+	image = models.ImageField()
+
+class Ingredient(models.Model):
+	value = models.CharField(max_length=150)
+	produce = models.ForeignKey(Produce)
+	recipe = models.ForeignKey(Recipe)
 
 # Create your models here.
 class CookPost(models.Model):
