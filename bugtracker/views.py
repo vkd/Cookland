49f3da4637aad96c843023b676c277462eb65d35
vkd <vkd.castle@gmail.com>
2014-09-28 12:11:04 +0400

    #13 bugtracker - move templates to namespacing folder

--- bugtracker/views.py
+++ bugtracker/views.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse_lazy
 # Create your views here.
 def index(request):
 	context = { }
-	return render(request, 'list_bug.html', context)
+	return render(request, 'bugs/list_bug.html', context)
 
 def revert_to_index():
 	pass
@@ -14,26 +14,25 @@ from .models import Ticket
 
 class BugListView(ListView):
     model = Ticket
-    template_name = 'list_bug.html'
-    success_url = reverse_lazy('456.html')
+    template_name = 'bugs/list_bug.html'
 
 from django.views.generic import DetailView
 
 class BugDetailView(DetailView):
     model = Ticket
-    template_name = 'detail_bug.html'
+    template_name = 'bugs/detail_bug.html'
 
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
-    template_name = 'register_bug.html'
+    template_name = 'bugs/register_bug.html'
     success_url = reverse_lazy('bugs:index')
 
 class BugCreateView(CreateView):
     model = Ticket
-    template_name = 'add_bug.html'
+    template_name = 'bugs/add_bug.html'
     fields = ['title', 'text']
     success_url = reverse_lazy('bugs:index')
 
