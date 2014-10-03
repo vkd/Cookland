cc6686cd3325a448a7522347e5bb29e56c9adf1e
vkd <vkd.castle@gmail.com>
2014-10-03 22:59:42 +0400

    #7 release auth

--- cookland/urls.py
+++ cookland/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import *
+from django.core.urlresolvers import reverse_lazy
 
 from cookland.views import *
 
@@ -7,5 +8,8 @@ urlpatterns = patterns('',
 	url(r'^admin/', form_input, name='admin'),
 	url(r'^recipes/', recipes_list, name='recipes'),
 	url(r'^404/', custom_not_found_page, name='404'),
+	url(r'^login/', login_action, name="login"),
+	url(r'^logout/$', 'django.contrib.auth.views.logout', 
+		{"next_page" : reverse_lazy('cook:index')}, name="logout"),
 	url(r'^$', index, name='index'),
 )
--- cookland/views.py
+++ cookland/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.template import loader, Context
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth import authenticate, login
 
 from cookland.models import *
 
@@ -31,4 +33,19 @@ def recipes_list(request):
 	context = { 'recipes' : Recipe.objects.all() }
 	return render(request, 'cook/recipes.html', context)
 	
+def login_action(request):
+	username = request.POST['login']
+	password = request.POST['password']
+	user = authenticate(username=username, password=password)
+	if user is not None:
+		if user.is_active:
+			login(request, user)
+			# Redirect to a success page.
+		else:
+			pass
+			# Return a 'disabled account' error message
+	else:
+		pass
+		# Return an 'invalid login' error message.
+	return HttpResponseRedirect(reverse_lazy('cook:index'))
 	
