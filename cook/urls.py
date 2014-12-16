34e9bfa08e4cf6e37e1fe253724bb3cdca5f4bd1
vkd <vkd.castle@gmail.com>
2014-12-16 23:35:55 +0300

    formatting

--- cook/urls.py
+++ cook/urls.py
@@ -3,15 +3,16 @@ from django.core.urlresolvers import reverse_lazy
 
 from cook.views import *
 
-urlpatterns = patterns('',
-	url(r'^recipe/(?P<pk>[0-9]+)/', view_recipe, name='view_recipe'),
-	url(r'^recipe/add/', add_recipe, name='add_recipe'),
-	url(r'^recipe/delete/(?P<pk>[0-9]+)/', delete_recipe, name='delete_recipe'),
-	url(r'^recipe/edit/(?P<pk>[0-9]+)/', edit_recipe, name='edit_recipe'),
-	url(r'^recipes/', recipes, name='recipes'),
-	url(r'^404/', custom_not_found_page, name='404'),
-	url(r'^login/', login_action, name="login"),
-	url(r'^logout/$', 'django.contrib.auth.views.logout',
-		{"next_page" : reverse_lazy('cook:index')}, name="logout"),
-	url(r'^$', index, name='index'),
+urlpatterns = patterns(
+    '',
+    url(r'^recipe/(?P<pk>[0-9]+)/', view_recipe, name='view_recipe'),
+    url(r'^recipe/add/', add_recipe, name='add_recipe'),
+    url(r'^recipe/delete/(?P<pk>[0-9]+)/', delete_recipe, name='delete_recipe'),
+    url(r'^recipe/edit/(?P<pk>[0-9]+)/', edit_recipe, name='edit_recipe'),
+    url(r'^recipes/', recipes, name='recipes'),
+    url(r'^404/', custom_not_found_page, name='404'),
+    url(r'^login/', login_action, name="login"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {"next_page": reverse_lazy('cook:index')}, name="logout"),
+    url(r'^$', index, name='index'),
 )
--- cookland/urls.py
+++ cookland/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-from cookland.views import home as cookland_index
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     url(r'^$', 'cookland.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
