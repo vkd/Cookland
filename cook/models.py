21f7d6b99cba5bca5661e60fa19c48dae6d891de
vkd <vkd.castle@gmail.com>
2014-12-18 22:09:23 +0300

    tab to spaces

--- cook/models.py
+++ cook/models.py
@@ -1,24 +1,28 @@
 from django.db import models
-from django.contrib import admin
+
 
 class Tag(models.Model):
-	name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150)
+
 
 class Produce(models.Model):
-	name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150)
+
 
 class Recipe(models.Model):
-	name = models.CharField(max_length=150)
-	ingredients = models.ManyToManyField(Produce, related_name='recipes', through='Ingredient')
-	discribe = models.TextField()
-	#image = models.ImageField(blank=True)
-	tags = models.ManyToManyField(Tag, related_name='recipes', through='Recipe_Tags')
+    name = models.CharField(max_length=150)
+    ingredients = models.ManyToManyField(Produce, related_name='recipes', through='Ingredient')
+    discribe = models.TextField()
+    #image = models.ImageField(blank=True)
+    tags = models.ManyToManyField(Tag, related_name='recipes', through='Recipe_Tags')
+
 
 class Recipe_Tags(models.Model):
-	recipe = models.ForeignKey(Recipe)
-	tag = models.ForeignKey(Tag)
+    recipe = models.ForeignKey(Recipe)
+    tag = models.ForeignKey(Tag)
+
 
 class Ingredient(models.Model):
-	value = models.CharField(max_length=150)
-	produce = models.ForeignKey(Produce)
-	recipe = models.ForeignKey(Recipe)
+    value = models.CharField(max_length=150)
+    produce = models.ForeignKey(Produce)
+    recipe = models.ForeignKey(Recipe)
